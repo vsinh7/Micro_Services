9736f2968b3241f3e08821c3d6b23607968c0524
VidyaBhushan <vsinha015@gmail.com>
2025-07-13 00:40:23 +0800

    Login and JWT Token
    Login and JWT Token

--- auth_micro/controller/AuthController.js
+++ auth_micro/controller/AuthController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt"
 import prisma from "../config/db.config.js"
+import jwt from "jsonwebtoken"
 
 class AuthController {
 
@@ -22,6 +23,41 @@ class AuthController {
             
         }
         
+    }
+
+    static async login(req, res){
+        try {
+            const {email ,password} = req.body;
+
+            const user = await prisma.user.findUnique({
+                where:{
+                    email: email
+                }
+            })
+            if(user){
+
+                if(!bcrypt.compareSync(password ,user.password)){
+                    return res.status(401).json({message:"Invalid Credentials"})
+                }
+
+                const payload = {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                };
+
+                const token = jwt.sign(payload ,process.env.JWT_SECRET_KEY, {
+                    expiresIn: "365d",
+                });
+
+                return res.json({message: "Login successfully", access_token: `Bearer ${token}`})
+
+            }
+
+           
+        } catch (error) {
+            return res.status(500).json({message:"Something went wrong! Please try again later"})
+        }
     }
 }
 
--- auth_micro/server.js
+++ auth_micro/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import Routes from "./routes/index.js"
+import "dotenv/config.js"
 
 
 const app = express()
